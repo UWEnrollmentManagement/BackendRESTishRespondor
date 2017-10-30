38e84e42214dad22e6a5c833e090d1fbe5c263f8
Liz <elizaw7@uw.edu>
2017-10-30 14:49:39 -0700

    Committing before pulling fro mmaster

--- test/FormsAPIFaker.php
+++ test/FormsAPIFaker.php
@@ -56,6 +56,9 @@ class FormsAPIFaker
                 'required' => "boolean",
                 'initial value' => 'catchPhrase',
             ],
+        ],
+        "visitors" => [
+
         ]
     ];
 
